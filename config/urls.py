9ed195eee45a014c686691ad35edef32109c1726
JJC <jjc@nlplab.cc>
2018-04-19 16:47:15 +0800

    adjust config

--- config/urls.py
+++ config/urls.py
@@ -13,22 +13,22 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# from django.contrib import admin
+from django.contrib import admin
 from django.urls import path
-from index import get_index
+# from index import get_index
 import paraphrase.views
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
-    path('example/', paraphrase.views.example),
-    path('index2/', paraphrase.views.index2),
-    path('translate/', paraphrase.views.translate),
-    path('wordlist/', paraphrase.views.wordlist),
-    path('similar/', paraphrase.views.similar),
-    path('extended/', paraphrase.views.extended),
-    path('pattern/', paraphrase.views.pattern),
+    path('admin/', admin.site.urls),
+    path('example/', paraphrase.views.get_example),
+    path('index2/', paraphrase.views.get_paraphrase2),
+    path('translate/', paraphrase.views.get_translation),
+    path('wordlist/', paraphrase.views.get_wordlist),
+    path('similar/', paraphrase.views.get_similar_paraphrase),
+    path('extended/', paraphrase.views.get_extended_paraphrase),
+    path('pattern/', paraphrase.views.get_pattern_paraphrase),
     path('preview/', paraphrase.views.preview),
     path('login/', paraphrase.views.login),
-    path('validate/', paraphrase.views.validate),
-    path('*', get_index),
+    path('validate/', paraphrase.views.validate_html),
+    path('', paraphrase.views.get_paraphrase),
 ]
